469b646436c5f3da19c2202ebfc0be918afbb1d3
adamrturman <adamrturman@gmail.com>
2020-09-16 13:22:59 -0500

    entries curl scripts are working. crud on a challenge works from backend

--- curl-scripts/entries/destroy.sh
+++ curl-scripts/entries/destroy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/entries"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
--- curl-scripts/entries/update.sh
+++ curl-scripts/entries/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/entries"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
@@ -9,8 +9,13 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Content-Type: application/json" \
 --header "Authorization: Bearer ${TOKEN}" \
 --data '{
-    "example": {
-      "text": "'"${TEXT}"'"
+    "entry": {
+      "title": "'"${TITLE}"'",
+      "description": "'"${DESC}"'",
+      "difficulty": "'"${DIFF}"'",
+      "link": "'"${LINK}"'",
+      "hint": "'"${HINT}"'",
+      "complexity": "'"${COMP}"'"
     }
   }'
 
