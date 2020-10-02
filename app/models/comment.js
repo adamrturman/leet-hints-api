75980667ae1336950b606da38175c558b58f7c25
adamrturman <adamrturman@gmail.com>
2020-10-02 13:59:23 -0500

    Revert "editted comment model"
    This reverts commit ac943a829f64d2a029c38965166ad4fddb01dd28.

--- app/models/comment.js
+++ app/models/comment.js
@@ -9,9 +9,6 @@ const commentSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
-  },
-  commentOwner: {
-    type: String
   }
 }, {
   timestamps: true
